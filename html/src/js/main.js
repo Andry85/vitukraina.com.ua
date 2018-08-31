5d309fa1c532f95e5cc970a62cb8168ea3c042de
HOME\Andry <shapovala@ukr.net>
2018-08-31 15:30:56 +0300

    added same pages

--- html/src/js/main.js
+++ html/src/js/main.js
@@ -39,7 +39,82 @@
     /********************************************************/
 
 
+    /*********************************************************/
+    /* mobile nav   */
+    /********************************************************/
+	    $('.menu').slicknav({
+			label: ''
+		});
+    /*********************************************************/
+    /* end mobile nav  */
+    /********************************************************/
 
+    mobileView();
+    function mobileView() {
+        
+        var wiw = window.innerWidth;
+        if(wiw < 992) {
+
+        	$(".languages li").each(function(){
+		        $(this).addClass('menu__item');
+		        $(this).find('a').attr("role","menuitem");
+		        $(this).find('a').attr("tabindex","0");
+		    });
+
+        	$('.languages').find('li').appendTo('.slicknav_nav');
+        }
+
+        if(wiw < 768) {
+
+        	 /*********************************************************/
+		    /* about slider   */
+		    /********************************************************/
+		        
+			        $('.gallerySlider .owl-carousel').owlCarousel({
+			            autoHeight:true,
+			            nav: false,
+			            dots: true,
+			            responsiveClass:true,
+			            items:1,
+			            margin: 0
+			        });
+
+		    /*********************************************************/
+		    /* end about slider  */
+		    /********************************************************/
+        }
+    };
+
+
+    /*********************************************************/
+    /* about slider   */
+    /********************************************************/
+        $('.aboutGallerySlider .owl-carousel').owlCarousel({
+            autoHeight:true,
+            nav: false,
+            dots: true,
+            responsiveClass:true,
+		    responsive:{
+		        0:{
+		            items:1,
+            		margin: 0
+		        },
+		        768:{
+		            items: 4,
+            		margin: 30,
+		        }
+		    }
+        });
+    /*********************************************************/
+    /* end about slider  */
+    /********************************************************/
+
+   
+
+
+
+
+    
     
 
 

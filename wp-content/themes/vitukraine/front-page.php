26155f15883dc2a121b22f9f8acfcad2e50c55ff
HOME\Andry <shapovala@ukr.net>
2018-09-08 12:51:13 +0300

    add fileds to the product page

--- wp-content/themes/vitukraine/front-page.php
+++ wp-content/themes/vitukraine/front-page.php
@@ -29,8 +29,6 @@ Template Name: Main Page
 								</div>
 							</div>
 
-
-
 					    <?php endwhile; ?>
 			    	</div>
 				   <?php else : ?>
@@ -38,7 +36,75 @@ Template Name: Main Page
 			</div>
 
 
-			<?php the_content(); ?>	
+			<?php $link_about = get_field('link_about'); ?>
+			<div class="aboutBlock">
+				<div class="container">
+					<div class="row">
+		  				<div class="col-xs-12">	
+							<h2 class="aboutBlock__title"><?php the_field('title_about'); ?></h2>
+							<p><?php the_field('text_about'); ?></p>
+							<a class="button" href="<?php echo $link_about['url']; ?>"><?php the_field('link_about_text'); ?></a>
+						</div>
+					</div>
+				</div>	
+			</div>
+
+
+			<div class="partnersBlock">
+				<div class="container">
+					<div class="row">
+		  				<div class="col-xs-12">	
+							<h2 class="partnersBlock__title"><?php the_field('partners_title'); ?></h2>
+							
+
+							<div class="partnersSlider">
+								<?php if( have_rows('partners_slider') ): ?>
+									<div class="owl-carousel">
+									    <?php while ( have_rows('partners_slider') ) : the_row();
+									    	$image = get_sub_field('image');
+									    	$title = get_sub_field('title');
+									     ?>
+									    	<div class="partnersSlider__item">
+								    			<figure class="partnersSlider__pic">
+													<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+													<figcaption class="partnersSlider__title">
+														<?php the_sub_field('title'); ?>
+													</figcaption>
+												</figure>
+											</div>
+									    <?php endwhile; ?>
+							    	</div>
+								   <?php else : ?>
+								<?php endif; ?>
+							</div>
+							<?php $partners_link_url = get_field('partners_link_url'); ?>
+							<a class="button" href="<?php echo $partners_link_url['url']; ?>">
+								<?php the_field('partners_link_text'); ?>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="contactsBlock">
+				<div class="contactsBlock__info">
+					<ul class="contactsBlockList">
+						<li>
+							<i class="icon icon-home-icon"></i>
+							<span><?php the_field('contacts_adress'); ?></span>
+						</li>
+						<li>
+							<i class="icon icon-phone-icon"></i>
+							<a href="tel:<?php the_field('contacts_phone_link'); ?>"><?php the_field('contacts_phone'); ?></a>
+						</li>
+					</ul>
+				</div>
+				<div class="contactsBlock__map">
+					<?php if( get_field('contacts_map') ): ?>
+						<img src="<?php the_field('contacts_map'); ?>" />
+					<?php endif; ?>
+				</div>
+			</div>
 		<?php endwhile; else : ?>
 			<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 		<?php endif; ?>
--- wp-content/themes/vitukraine/products.php
+++ wp-content/themes/vitukraine/products.php
@@ -20,7 +20,47 @@ Template Name: Products
 						<div class="row">
 							<div class="col-xs-12">
 								<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-									<?php the_content(); ?>	
+									<?php the_content(); ?>
+
+									<div class="productInfo">
+										<div class="row">
+											<div class="col-sm-5">
+												<figure class="productInfo__pic">
+													<?php if( get_field('image_left_block') ): ?>
+														<img src="<?php the_field('image_left_block'); ?>" />
+													<?php endif; ?>
+												</figure>
+											</div>
+											<div class="col-sm-6">
+												<div class="productInfo__text">
+													<?php the_field('text_left_block'); ?>
+												</div>
+											</div>
+										</div>
+									</div>	
+
+									<div class="certificate">
+										<div class="row">
+											<div class="col-sm-5 col-sm-offset-1">
+												<div class="certificate__text">
+													<?php the_field('text_right_block'); ?>
+												</div>
+											</div>
+											<div class="col-sm-4 col-sm-offset-1">
+												<figure class="certificate__pic">
+													<?php if( get_field('image_link_right_block') ): ?>
+														<a href="<?php the_field('image_link_right_block'); ?>">
+															<?php if( get_field('image_right_block') ): ?>
+																<img src="<?php the_field('image_right_block'); ?>" />
+															<?php endif; ?>
+														</a>
+													<?php endif; ?>	
+												</figure>
+											</div>
+										</div>
+									</div>	
+
+
 								<?php endwhile; else : ?>
 									<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 								<?php endif; ?>
